fc2839525acd6bfd9674674bbfdc3e39da7fa4ab
c00p75_ <georgecoopmsapenda@gmail.com>
2023-03-23 11:33:50 +0200

    Add calculator logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,60 @@
-const Calculator = () => (
-  <div className="container text-center mt-5" id="calc-container">
-    <div className="row g-2">
-      <div className="col d-flex justify-content-end align-items-center" id="result">0</div>
-    </div>
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 
-    <div className="row justify-content-center align-items-center g-2">
-      <button type="button" className="col">AC</button>
-      <button type="button" className="col">+/-</button>
-      <button type="button" className="col">%</button>
-      <button type="button" className="col">÷</button>
-    </div>
+// Refactoring class component to function component which uses a React hook
+const Calculator = () => {
+  // initializing current state `initCalc` and function that updates the state `setCalculater`
+  const [initCalc, setCalculator] = useState({ total: null, next: null, operation: null });
 
-    <div className="row justify-content-center align-items-center g-2">
-      <button type="button" className="col">7</button>
-      <button type="button" className="col">8</button>
-      <button type="button" className="col">9</button>
-      <button type="button" className="col">x</button>
-    </div>
+  // Defining function to handle click events and update state
+  const handleClick = (event) => {
+    // Changes initial calculator state by calling imported `calculate` function
+    setCalculator(calculate(initCalc, event.target.innerHTML));
+  };
+  // Definfing HTML that should be rendered to the DOM
+  return (
+    <div className="container text-center mt-5" id="calc-container">
+      <div className="row g-2">
+        <div className="col d-flex justify-content-end align-items-center" id="result">
+          {initCalc.next || initCalc.operation || initCalc.total || 0}
+        </div>
+      </div>
 
-    <div className="row justify-content-center align-items-center g-2">
-      <button type="button" className="col">4</button>
-      <button type="button" className="col">5</button>
-      <button type="button" className="col">6</button>
-      <button type="button" className="col">-</button>
-    </div>
+      <div className="row justify-content-center align-items-center g-2">
+        <button onClick={handleClick} type="button" className="col">AC</button>
+        <button onClick={handleClick} type="button" className="col">+/-</button>
+        <button onClick={handleClick} type="button" className="col">%</button>
+        <button onClick={handleClick} type="button" className="col">÷</button>
+      </div>
 
-    <div className="row justify-content-center align-items-center g-2">
-      <button type="button" className="col">1</button>
-      <button type="button" className="col">2</button>
-      <button type="button" className="col">3</button>
-      <button type="button" className="col">+</button>
-    </div>
+      <div className="row justify-content-center align-items-center g-2">
+        <button onClick={handleClick} type="button" className="col">7</button>
+        <button onClick={handleClick} type="button" className="col">8</button>
+        <button onClick={handleClick} type="button" className="col">9</button>
+        <button onClick={handleClick} type="button" className="col">x</button>
+      </div>
 
-    <div className="row justify-content-center align-items-center g-2">
-      <button type="button" className="col-6">0</button>
-      <button type="button" className="col">.</button>
-      <button type="button" className="col">=</button>
+      <div className="row justify-content-center align-items-center g-2">
+        <button onClick={handleClick} type="button" className="col">4</button>
+        <button onClick={handleClick} type="button" className="col">5</button>
+        <button onClick={handleClick} type="button" className="col">6</button>
+        <button onClick={handleClick} type="button" className="col">-</button>
+      </div>
+
+      <div className="row justify-content-center align-items-center g-2">
+        <button onClick={handleClick} type="button" className="col">1</button>
+        <button onClick={handleClick} type="button" className="col">2</button>
+        <button onClick={handleClick} type="button" className="col">3</button>
+        <button onClick={handleClick} type="button" className="col">+</button>
+      </div>
+
+      <div className="row justify-content-center align-items-center g-2">
+        <button onClick={handleClick} type="button" className="col-6">0</button>
+        <button onClick={handleClick} type="button" className="col">.</button>
+        <button onClick={handleClick} type="button" className="col">=</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
