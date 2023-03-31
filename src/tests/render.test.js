b7f354af20db87527040839aa85f651ded1b2c67
SaneMyburg <sane.myburg@gmail.com>
2023-03-31 07:53:04 +0200

    Fix linter errors

--- src/tests/render.test.js
+++ src/tests/render.test.js
@@ -4,6 +4,7 @@ import Home from '../components/Home';
 import Calculator from '../components/Calculator';
 import Navigation from '../components/Navbar';
 import Quote from '../components/Quote';
+
 it('Test if Home is correctly rendered', () => {
   const tree = renderer.create(<Home />).toJSON();
   expect(tree).toMatchSnapshot();
